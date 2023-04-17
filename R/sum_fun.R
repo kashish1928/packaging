8d34b8ece2a84860bc06c1d3055819b997466304
kashish1928 <kashishjoshipura@gmail.com>
2023-04-17 10:21:30 -0700

    documentation

--- R/sum_fun.R
+++ R/sum_fun.R
@@ -1,3 +1,15 @@
+#' Perform addition
+#'
+#' @param x A numeric variable
+#' @param y A numeric variable
+#'
+#' @return A numeric variable
+#' @export
+#'
+#' @examples
+#' x <- 3
+#' y <- 4
+#' sum_fun(x,y)
 sum_fun <- function(x,y) {
   if(is.numeric(x) && is.numeric(y)) {
     x + y
